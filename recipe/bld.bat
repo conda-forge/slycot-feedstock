534d23a01fbdba9b3b0f5b98f7d89b0dac0a4ae3
Rene van Paassen <m.m.vanpaassen@tudelft.nl>
2019-04-15 17:34:40 +0200

    set path to P2PY

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ set LAPACK_ROOT=%PREFIX%
 
 :: correct FC, apparently pointed to host prefix??
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
+set F2PY=%BUILD_PREFIX%\Scripts\f2py.exe
 
 "%PYTHON%" setup.py install
 
