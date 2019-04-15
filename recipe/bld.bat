0621dce4010a75899ee8e89f22bf7f8d2572201d
Rene van Paassen <m.m.vanpaassen@tudelft.nl>
2019-04-15 12:48:47 +0200

    use updated slycot, that does not detect cxx fix FC environment variable for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
 set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
 
+:: correct FC, apparently pointed to host prefix??
+set FC=%BUILD_PREFIX%\Library\bin\flang.exe
+
 "%PYTHON%" setup.py install
 
 if errorlevel 1 exit 1
