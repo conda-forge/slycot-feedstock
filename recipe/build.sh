0b86d1bda00ef6197fe88ab9a53b99fdf497657b
Isuru Fernando <isuruf@gmail.com>
2019-08-31 16:21:21 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,4 +10,7 @@ if [[ "$target_platform" == "osx-64" ]]; then
   fi
 fi
 
+# Add a openblas link to avoid linking to accelerate and other system libraries.
+ln -s $PREFIX/lib/liblapack${SHLIB_EXT} $PREFIX/lib/libopenblas${SHLIB_EXT}
 $PYTHON setup.py build_ext -llapack -lblas install
+rm $PREFIX/lib/libopenblas${SHLIB_EXT}
