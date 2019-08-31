b0cbbd5e473f97f6326df0ce0ddc5697b02c2f64
Isuru Fernando <isuruf@gmail.com>
2019-08-31 15:55:29 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,1 +1,6 @@
+if [[ "$target_platform" == "osx-64" ]]; then
+    # Remove when https://github.com/scikit-build/scikit-build/pull/441 lands
+    sed -i.bak "s/'-DSLYCOT_VERSION/'-DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET', '-DSLYCOT_VERSION/g" setup.py
+fi
+
 $PYTHON setup.py build_ext -llapack -lblas install
