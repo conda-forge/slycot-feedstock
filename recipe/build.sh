4abefeba84b88149fa848c89e9bffb153873b375
Isuru Fernando <isuruf@gmail.com>
2020-01-25 12:51:57 -0600

    Try setting sysroot

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 export LDFLAGS="$LDFLAGS -v"
+if [[ "$target_platform" == osx-64 ]]; then
+  export LDFLAGS="$LDFLAGS --sysroot=$CONDA_BUILD_SYSROOT"
+fi
 # Add a openblas link to avoid linking to accelerate and other system libraries.
 ln -s $PREFIX/lib/liblapack${SHLIB_EXT} $PREFIX/lib/libopenblas${SHLIB_EXT}
 $PYTHON setup.py build_ext -llapack -lblas install
