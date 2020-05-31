41e895fa882b98d977b5e237687b5e997edb4299
René van Paassen <rene.vanpaassen@gmail.com>
2020-05-31 17:33:12 +0200

    select Generic for BLAS vendor, to force OSX build with lapack

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,3 @@
-export LDFLAGS="$LDFLAGS -v"
-if [[ "$target_platform" == osx-64 ]]; then
-  export LDFLAGS="$LDFLAGS --sysroot=$CONDA_BUILD_SYSROOT"
-fi
-# Add a openblas link to avoid linking to accelerate and other system libraries.
-ln -s $PREFIX/lib/liblapack${SHLIB_EXT} $PREFIX/lib/libopenblas${SHLIB_EXT}
-$PYTHON setup.py build_ext -llapack -lblas install
-rm $PREFIX/lib/libopenblas${SHLIB_EXT}
+$PYTHON setup.py build_ext install -- \
+        -DNumPy_INCLUDE_DIR=${SP_DIR}/numpy/core/include \
+        -DBLA_VENDOR=Generic  -DF2PY=${PREFIX}/bin/f2py
