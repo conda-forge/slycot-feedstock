115aa25f5d2071e8d7c9022859691c37de222a76
René van Paassen <rene.vanpaassen@gmail.com>
2020-05-31 18:02:28 +0200

    F2PY define not needed & not effective, removing

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,3 @@
 $PYTHON setup.py build_ext install -- \
         -DNumPy_INCLUDE_DIR=${SP_DIR}/numpy/core/include \
-        -DBLA_VENDOR=Generic  -DF2PY=${PREFIX}/bin/f2py
+        -DBLA_VENDOR=Generic
