dae5a28b0cdd1481f44fb7395c7f57bc6274c0a3
Ben Greiner <code@bnavigator.de>
2021-11-10 14:03:13 +0100

    use bld.bat and recipe from python-control/Slycot repo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,6 @@
-
-:: correct FC, apparently pointed to host prefix??
-set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
-if exist %PREFIX%\Scripts\f2py.exe (
-  set F2PY=%PREFIX%\Scripts\f2py.exe
-) else (
-  set F2PY=%PREFIX%\Scripts\f2py.bat
-)
 
-"%PYTHON%" setup.py install
+"%PYTHON%" setup.py install -G "NMake Makefiles" -DBLA_VENDOR=Generic
 
 if errorlevel 1 exit 1
