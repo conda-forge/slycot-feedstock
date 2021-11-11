470774fcac94ea3b834ecaaf83f861f2d47c0c84
Ben Greiner <code@bnavigator.de>
2021-11-11 20:09:04 +0100

    remove hidden character

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 :: correct FC, apparently pointed to host prefix??
-￼set FC=%BUILD_PREFIX%\Library\bin\flang.exe
+set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
 
