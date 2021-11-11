d5872cd0aa5dfe958834d0fa2451bba1447e8f60
Ben Greiner <code@bnavigator.de>
2021-11-11 20:53:04 +0100

    debug cmake path and executable

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
+# debug line
+which cmake
+cmake --version
+echo "OK"
+#
 $PYTHON setup.py build_ext install -- \
         -DNumPy_INCLUDE_DIR=${SP_DIR}/numpy/core/include \
         -DBLA_VENDOR=Generic
