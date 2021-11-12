cb8a6e83022913fe2f5daec15da0cf25e1494504
Ben Greiner <code@bnavigator.de>
2021-11-12 18:11:01 +0100

    use CMAKE_ARGS, required by osx-arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,4 @@
-# debug line
-which cmake
-$BUILD_PREFIX/bin/cmake --version
-cmake --version
-echo "OK"
-#
 $PYTHON setup.py build_ext install -- \
         -DNumPy_INCLUDE_DIR=${SP_DIR}/numpy/core/include \
-        -DBLA_VENDOR=Generic
+        -DBLA_VENDOR=Generic \
+        ${CMAKE_ARGS}
