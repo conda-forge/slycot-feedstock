b88104e2997c33b2e276fbd77007c5e2bc5498a1
Ben Greiner <code@bnavigator.de>
2021-12-28 12:00:33 +0100

    debug build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 # override scikit-build handling for macOS arm platforn on conda-forge cross-compiling
-if [ $target_platform = "osx-arm4" ]; then
+echo $target_platform
+if [ "$target_platform" = "osx-arm4" ]; then
   export MACOSX_DEPLOYMENT_TARGET="arm64"
 fi
+echo MACOSX_DEPLOYMENT_TARGET
 $PYTHON setup.py build_ext install -- \
         -DBLA_VENDOR=Generic
