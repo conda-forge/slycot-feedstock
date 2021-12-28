d478510ecea1a9d8310581ce3b667de1288690f6
Ben Greiner <code@bnavigator.de>
2021-12-28 12:20:24 +0100

    fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
 # override scikit-build handling for macOS arm platforn on conda-forge cross-compiling
 echo $target_platform
-if [ "$target_platform" = "osx-arm4" ]; then
+if [ "$target_platform" = "osx-arm64" ]; then
   export MACOSX_DEPLOYMENT_TARGET="arm64"
 fi
-echo MACOSX_DEPLOYMENT_TARGET
+echo $MACOSX_DEPLOYMENT_TARGET
 $PYTHON setup.py build_ext install -- \
         -DBLA_VENDOR=Generic
