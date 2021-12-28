3aea7706f66948325f27804bd48ca1d1630da14a
Ben Greiner <code@bnavigator.de>
2021-12-28 11:22:47 +0100

    override skbuild target platform handling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,6 @@
+# override scikit-build handling for macOS arm platforn on conda-forge cross-compiling
+if [ $target_platform = "osx-arm4" ]; then
+  export MACOSX_DEPLOYMENT_TARGET="arm64"
+fi
 $PYTHON setup.py build_ext install -- \
         -DBLA_VENDOR=Generic
