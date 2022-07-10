c905bb458692eba4be3754ff4e130db0eb8fa7c4
Ben Greiner <code@bnavigator.de>
2022-07-10 22:19:14 +0200

    remove OS-X ARM workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,6 @@
 # override scikit-build handling for macOS arm platforn on conda-forge cross-compiling
 if [ "$target_platform" = "osx-arm64" ]; then
   export CMAKE_OSX_ARCHITECTURES="arm64"
-  # workaround for https://github.com/scikit-build/scikit-build/issues/589
-  rm -rf $PREFIX/lib/libpython$PY_VER.dylib
-  ln -sf $PREFIX/lib/libc++.dylib $PREFIX/lib/libpython$PY_VER.dylib
 fi
 
 export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic"
