648ca74a9f50599e964ced4e9a62a3ac7edeeb8b
Ben Greiner <code@bnavigator.de>
2022-07-12 10:29:06 +0200

    override F2PY_EXECUTABLE

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,15 @@
-# override scikit-build handling for macOS arm platforn on conda-forge cross-compiling
+# override scikit-build handling for macOS arm platform on conda-forge cross-compiling
 if [ "$target_platform" = "osx-arm64" ]; then
   export CMAKE_OSX_ARCHITECTURES="arm64"
 fi
 
-export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic"
+# fix entry_point -- https://github.com/conda-forge/numpy-feedstock/issues/276
+# Uses the python inside the pip build environment
+cat > myf2py.sh <<EOF
+#!/bin/sh
+python -m numpy.f2py.__main__ \$@
+EOF
+chmod +x myf2py.sh
+export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic -DF2PY_EXECUTABLE=$PWD/myf2py.sh"
+
 $PYTHON -m pip install -v .
