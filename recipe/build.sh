ce7779222381f06ea8730870a2b3b6bf347d4ae9
Ben Greiner <code@bnavigator.de>
2022-07-13 21:31:51 +0200

    TEST: new f2py entry points by numpy dev channel

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,9 @@ if [ "$target_platform" = "osx-arm64" ]; then
   export CMAKE_OSX_ARCHITECTURES="arm64"
 fi
 
-# fix entry_point -- https://github.com/conda-forge/numpy-feedstock/issues/276
-# Uses the python inside the pip build environment
-cat > myf2py.sh <<EOF
-#!/bin/sh
-python -m numpy.f2py.__main__ \$@
-EOF
-chmod +x myf2py.sh
-export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic -DF2PY_EXECUTABLE=$PWD/myf2py.sh"
+export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic"
+
+which f2py3
+f2py3 -h
 
 $PYTHON -m pip install -v .
