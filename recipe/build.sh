076087246621b0694f9f36ddc2ada62d2df8beb3
Ben Greiner <code@bnavigator.de>
2023-12-03 21:50:09 +0100

    try ROOT_DIR and add debug line

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,9 @@ if [ "$target_platform" = "osx-arm64" ]; then
 fi
 # fix scikit-build not finding the cross-compile python headers.
 if [ "$target_platform" != "$build_platform" ]; then
-  crosspython="-DPython_FIND_VIRTUALENV=ONLY"
+  pyrootdir=$($PYTHON -c 'import sysconfig; print(sysconfig.get_path("stdlib"))')
+  crosspython="-DPython_ROOT_DIR=$pyrootdir)"
 fi
-
 # fix entry_point -- https://github.com/conda-forge/numpy-feedstock/issues/276
 # Uses the python inside the pip build environment
 cat > myf2py.sh <<EOF
@@ -15,5 +15,7 @@ python -m numpy.f2py.__main__ \$@
 EOF
 chmod +x myf2py.sh
 export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic -DF2PY_EXECUTABLE=$PWD/myf2py.sh $crosspython"
+
+echo $SKBUILD_CONFIGURE_OPTIONS
 
 $PYTHON -m pip install -v .
