0ad8366d8c142115abcf836f0e9478904b7255f0
Ben Greiner <code@bnavigator.de>
2023-12-03 22:01:32 +0100

    use both, fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,10 @@
 if [ "$target_platform" = "osx-arm64" ]; then
   export CMAKE_OSX_ARCHITECTURES="arm64"
 fi
-# fix scikit-build not finding the cross-compile python headers.
+# fix scikit-build not finding the cross-compile python.
 if [ "$target_platform" != "$build_platform" ]; then
   pyrootdir=$($PYTHON -c 'import sysconfig; print(sysconfig.get_path("stdlib"))')
-  crosspython="-DPython_ROOT_DIR=$pyrootdir)"
+  crosspython="-DPython_ROOT_DIR=$pyrootdir -DPython_FIND_VIRTUALENV=ONLY"
 fi
 # fix entry_point -- https://github.com/conda-forge/numpy-feedstock/issues/276
 # Uses the python inside the pip build environment
@@ -16,6 +16,6 @@ EOF
 chmod +x myf2py.sh
 export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic -DF2PY_EXECUTABLE=$PWD/myf2py.sh $crosspython"
 
-echo $SKBUILD_CONFIGURE_OPTIONS
+echo SKBUILD_CONFIGURE_OPTIONS=$SKBUILD_CONFIGURE_OPTIONS
 
 $PYTHON -m pip install -v .
