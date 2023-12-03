8a839298776cc7d6ead0b3c226d64d9457704ee9
Ben Greiner <code@bnavigator.de>
2023-12-03 21:21:16 +0100

    cross-compile and force cmake Python_FIND_VIRTUALENV

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 if [ "$target_platform" = "osx-arm64" ]; then
   export CMAKE_OSX_ARCHITECTURES="arm64"
 fi
+# fix scikit-build not finding the cross-compile python headers.
+if [ "$target_platform" != "$build_platform" ]; then
+  crosspython="-DPython_FIND_VIRTUALENV=ONLY"
+fi
 
 # fix entry_point -- https://github.com/conda-forge/numpy-feedstock/issues/276
 # Uses the python inside the pip build environment
@@ -10,6 +14,6 @@ cat > myf2py.sh <<EOF
 python -m numpy.f2py.__main__ \$@
 EOF
 chmod +x myf2py.sh
-export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic -DF2PY_EXECUTABLE=$PWD/myf2py.sh"
+export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic -DF2PY_EXECUTABLE=$PWD/myf2py.sh $crosspython"
 
 $PYTHON -m pip install -v .
