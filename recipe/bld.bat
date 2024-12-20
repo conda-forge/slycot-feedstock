349ea1ec5b5f691bb52e654eab497cc244c6f67f
Ben Greiner <code@bnavigator.de>
2024-12-20 10:27:26 +0100

    Use Ninja with win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
 
 set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
-set "CMAKE_GENERATOR=NMake Makefiles"
+set "CMAKE_GENERATOR=Ninja"
 "%PYTHON%" -m pip install -v .
 
 if errorlevel 1 exit 1
